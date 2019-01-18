11467cbd74835adda0f9b29fd93966a85760e03c
Harrison Feng <feng.harrison@gmail.com>
2019-01-18 16:13:26 +0800

    change indentation level
    Signed-off-by: Harrison Feng <feng.harrison@gmail.com>

--- ml_w_pyspark.py
+++ ml_w_pyspark.py
@@ -21,65 +21,65 @@ def price_range(brand):
 
 def main():
 
-   spark = SparkSession.builder.appName('Data Processing').getOrCreate()
-   df = spark.read.csv('/tmp/sample_data.csv', inferSchema=True, header=True)
-   brand_udf = udf(price_range, StringType())
+    spark = SparkSession.builder.appName('Data Processing').getOrCreate()
+    df = spark.read.csv('/tmp/sample_data.csv', inferSchema=True, header=True)
+    brand_udf = udf(price_range, StringType())
 
-   LOG.info("Columns: {0}".format(df.columns))
-   LOG.info("Count: {0}".format(df.count()))
+    LOG.info("Columns: {0}".format(df.columns))
+    LOG.info("Count: {0}".format(df.count()))
 
-   # Check schema of dataframe.
-   df.printSchema()
+    # Check schema of dataframe.
+    df.printSchema()
 
-   # Read two columns using select method.
-   df.select('age', 'mobile').show()
-   
-   # decribe for analyzing the dataframe.
-   df.describe().show()
+    # Read two columns using select method.
+    df.select('age', 'mobile').show()
 
-   # Add a new column.
-   df.withColumn('age_after_10_yrs', (df['age'] + 10)).show(10, False)
+    # decribe for analyzing the dataframe.
+    df.describe().show()
 
-   # Add a new column and change the datatype of column.
-   df.withColumn('age_double', df['age'].cast(DoubleType())).show(10, False)
+    # Add a new column.
+    df.withColumn('age_after_10_yrs', (df['age'] + 10)).show(10, False)
 
-   # Filtering data
-   df.filter(df['mobile'] == 'Vivo').select('age', 'ratings', 'mobile').show()
- 
-   # Filtering data
-   df.filter(df['mobile'] == 'Vivo').filter(df['experience'] > 10).show()
+    # Add a new column and change the datatype of column.
+    df.withColumn('age_double', df['age'].cast(DoubleType())).show(10, False)
 
-   # Distinct values in Column
-   df.select('mobile').distinct().show()
+    # Filtering data
+    df.filter(df['mobile'] == 'Vivo').select('age', 'ratings', 'mobile').show()
 
-   # Count
-   LOG.info("Distinct values count: {0}".format(df.select('mobile').distinct().count()))
+    # Filtering data
+    df.filter(df['mobile'] == 'Vivo').filter(df['experience'] > 10).show()
 
+    # Distinct values in Column
+    df.select('mobile').distinct().show()
 
-   # Group by
-   df.groupBy('mobile').count().show(10, False)
+    # Count
+    LOG.info("Distinct values count: {0}".format(
+        df.select('mobile').distinct().count()))
 
+    # Group by
+    df.groupBy('mobile').count().show(10, False)
 
-   # Order by
-   df.groupBy('mobile').count().orderBy('count', ascending=False).show(10, False)
+    # Order by
+    df.groupBy('mobile').count().orderBy('count', ascending=False).show(10,
+                                                                        False)
 
-   # Mean, Sum and Min values
-   df.groupBy('mobile').mean().show(10, False) 
-   df.groupBy('mobile').sum().show(10, False)
-   df.groupBy('mobile').max().show(10, False)
-   df.groupBy('mobile').min().show(10, False)
+    # Mean, Sum and Min values
+    df.groupBy('mobile').mean().show(10, False)
+    df.groupBy('mobile').sum().show(10, False)
+    df.groupBy('mobile').max().show(10, False)
+    df.groupBy('mobile').min().show(10, False)
 
-   # Aggregations
-   df.groupBy('mobile').agg({'experience': 'sum'}).show(10, False)
+    # Aggregations
+    df.groupBy('mobile').agg({'experience': 'sum'}).show(10, False)
 
-   # UDF
-   df.withColumn('price_range', brand_udf(df['mobile'])).show(10, False)
-   
-   # lambda udf
-   age_udf = udf(lambda age: "young" if age <= 30 else "senior", StringType())
-   df.withColumn("age_group", age_udf(df['age'])).show(10, False)
+    # UDF
+    df.withColumn('price_range', brand_udf(df['mobile'])).show(10, False)
+
+    # lambda udf
+    age_udf = udf(lambda age: "young" if age <= 30 else "senior", StringType())
+    df.withColumn("age_group", age_udf(df['age'])).show(10, False)
 
 
 if __name__ == '__main__':
-   logging.basicConfig(level=logging.INFO)
-   main()
+    logging.basicConfig(level=logging.INFO)
+    main()
